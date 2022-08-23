f8e8eaaa310f97b4caadf7305d0bef7f670a3f3a
Julian Zücker <71923164+julianzue@users.noreply.github.com>
2022-08-23 11:55:41 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,21 @@
+module example/player
+
+go 1.19
+
+require (
+	github.com/faiface/beep v1.1.0
+	github.com/fatih/color v1.13.0
+)
+
+require (
+	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
+	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
---
+++ player.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/mitchellh/colorstring"
+)
+
+func main() {
+
+	var path string
+
+	colorstring.Print("[yellow][+] [white]Enter Absolute Path: ")
+	fmt.Scan(&path)
+
+	f, _ := os.Open(path)
+
+	filesize, _ := os.Stat(path)
+
+	streamer, format, _ := mp3.Decode(f)
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	speaker.Play(streamer)
+
+	full := strings.Split(path, "/")
+
+	s := fmt.Sprintf("%.2f", float64(filesize.Size())/(1024*1000))
+
+	title := full[len(full)-1]
+
+	colorstring.Println("[light_cyan][*] [white]Playing:[light_cyan] " + title + " [white](" + s + "MB)")
+
+	for {
+	}
+
+}
